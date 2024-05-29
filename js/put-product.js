a56ab9a35711da4424290269c71e0e040f63795f
João Pedro <joaopedropalazzolli@gmail.com>
2024-05-29 16:01:46 -0300

    Criado função para atualizar produto

---
+++ js/put-product.js
@@ -0,0 +1,50 @@
+import { refreshToken } from "./refresh.js";
+
+const btn = document.getElementById('editarProduct');
+const nomeProduto = document.getElementById('produto');
+const descricao = document.getElementById('descricao');
+const quantidade = document.getElementById('quantidade');
+const dataValidade = document.getElementById('date');
+const tipo = document.getElementById('selecttype');
+const lembrete = document.getElementById('selectime');
+
+const urlCompleta = window.location.href;
+const url = new URL(urlCompleta);
+const urlSearch = new URLSearchParams(url.search);
+const id = urlSearch.get("id");
+
+const dados = JSON.parse(localStorage.getItem('local'));
+
+const serverURL = 'http://localhost:8080/api/v1/product';
+
+btn.addEventListener('click', () => {
+  fetch(serverURL,
+    {
+      headers: {
+        "Accept": "*/*",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + dados.accessToken
+      },
+      method: "PUT",
+      body: JSON.stringify({
+        id: id,
+        nomeProduto: nomeProduto.value,
+        descricao: descricao.value,
+        quantidade: quantidade.value,
+        dataValidade: dataValidade.value,
+        timerLembrete: lembrete.value,
+        tipo: tipo.value
+      })
+    })
+    .then(function (res) {
+      console.log(res)
+      if (res.status >= 200 && res.status <= 299) {
+        window.location.href = 'index.html'
+      }
+      if (res.status == 403) {
+        refreshToken();
+      }
+    })
+    .catch(function (res) { console.log(res) })
+});
+
